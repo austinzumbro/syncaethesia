f2457f69d1caae31f3035c130cd9065dbe5aa0e4
Austin Zumbro <azumbro@gmail.com>
2023-05-09 13:59:28 -0700

    create new user working

--- controllers/spotifyRoute.js
+++ controllers/spotifyRoute.js
@@ -1,15 +1,17 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const dotenv = require('dotenv');
+// const SpotifyWebApi = require('spotify-web-api-node');
+// const dotenv = require('dotenv');
 const router = require('express').Router();
 
 const { User } = require('../models');
+const spotifyApi = require('../config/spotify-config');
+const { spotifyAuth } = require('../utils/spotify-auth');
 
 // credentials are optional
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOT_CLIENT_ID,
-  clientSecret: process.env.SPOT_CLIENT_SECRET,
-  redirectUri: 'http://localhost:3001/spotify/callback',
-});
+// const spotifyApi = new SpotifyWebApi({
+//   clientId: process.env.SPOT_CLIENT_ID,
+//   clientSecret: process.env.SPOT_CLIENT_SECRET,
+//   redirectUri: 'http://localhost:3001/spotify/callback',
+// });
 
 //creating authorize url
 const scopes = [
@@ -36,36 +38,31 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/callback', async (req, res) => {
-  const code = req.query.code;
-  const data = await spotifyApi.authorizationCodeGrant(code);
+router.get('/callback', spotifyAuth, async (req, res) => {
+  const currentUser = await spotifyApi.getMe();
 
-  console.log('The token expires in ' + data.body['expires_in']);
-  console.log('The access token is ' + data.body['access_token']);
-  console.log('The refresh token is ' + data.body['refresh_token']);
-
-  spotifyApi.setAccessToken(data.body['access_token']);
-  spotifyApi.setRefreshToken(data.body['refresh_token']);
-
-  /*
-  // grab spotify info
-  const { body: { id , display_name, email} } = await spotifyApi.getMe();
-  const userExists = await User.findByPk(userId);
-  if(userExists){
-    res.redirect('/dashboard');
-    return;
-  } 
-
-  //create new user in database
-  const newUser = User.create({ 
-    id: id,
-    display_name: display_name,
-    email: email
+  const userExists = await User.findAll({
+    where: {
+      spotify_id: currentUser.body.id,
+    },
   });
 
-  
-  */
-  res.redirect('/dashboard');
+  if (!userExists[0]) {
+    console.log('User does not exist.');
+
+    const newUser = await User.create({
+      spotify_id: currentUser.body.id,
+      display_name: currentUser.body.display_name,
+      email: currentUser.body.email,
+    });
+
+    console.log(newUser);
+  } else {
+    console.log('User exists.');
+  }
+
+  res.status(200).json(currentUser);
+  //('/dashboard');
 });
 
 // methods for grabbing spotify info
@@ -74,17 +71,20 @@ router.get('/callback', async (req, res) => {
 // get current logged in users info
 const getMe = async () => {
   try {
-    const { body: { userId } } = await spotifyApi.getMe();
+    const {
+      body: { userId },
+    } = await spotifyApi.getMe();
     console.log('Some information about the authenticated user', userId);
     return userId; // return the user id for later reference
   } catch (err) {
     console.log('Something went wrong!', err);
     throw err; // re-throw the error to the calling code
   }
-}
+};
 
 // Get a user's playlists
-const getUserPlaylists = async (userId) => { //usually takes in string parameter to search by user but i put id to see more specific search might not work
+const getUserPlaylists = async (userId) => {
+  //usually takes in string parameter to search by user but i put id to see more specific search might not work
   try {
     const { body } = await spotifyApi.getUserPlaylists(userId);
     console.log('Retrieved playlists', body);
@@ -94,11 +94,5 @@ const getUserPlaylists = async (userId) => { //usually takes in string parameter
     throw err;
   }
 };
-
-
-
-
-
-
 
 module.exports = router;
