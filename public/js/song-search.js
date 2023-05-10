0e36171f0421d76e09c660f9e6e449ff154bc2c3
Austin Zumbro <azumbro@gmail.com>
2023-05-09 21:30:19 -0700

    song search form handler added

--- public/js/song-search.js
+++ public/js/song-search.js
@@ -1,4 +1,5 @@
 const searchForm = document.querySelector('#song-search-form');
+const songListArea = document.querySelector('#song-list-area');
 
 const runSearch = async (event) => {
   event.preventDefault();
@@ -8,15 +9,38 @@ const runSearch = async (event) => {
 
   console.log(trackName, artistName);
 
+  let testArray = [
+    {
+      spotify_id: 'spotify:track:0obBFrPYkSoBJbvHfUIhkv',
+      title: trackName,
+      artist: artistName,
+    },
+  ];
+
+  presentSongList(testArray);
+
   // const response = await fetch('/api/songs/search', {
   //   method: 'POST',
   //   body: JSON.stringify({
   //     track: trackName,
+  //     artist: artistName,
   //   }),
   //   headers: { 'Content-Type': 'application/json' },
   // });
 
   // console.log(response);
+};
+
+const presentSongList = (arr) => {
+  songListArea.innerHTML = '';
+  for (let i = 0; arr.length < i; i++) {
+    const songDiv = document.createElement('div');
+    songDiv.classList.add('bg-white', 'p-3', 'mt-3');
+    songDiv.setAttribute('data-spotify_id', `${arr[i].spotify_id}`);
+    songDiv.innerHTML = `<span>${arr[i].title}</span>
+    <span>${arr[i].artist}</span><button data-spotify_id=${arr[i].spotify_id} class='add-song-button'>Add to Playlist</button>`;
+    songListArea.appendChild(songDiv);
+  }
 };
 
 searchForm.addEventListener('submit', runSearch);
