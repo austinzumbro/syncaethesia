6566cbb6930e74d3e473f705b1f8e18068ec1307
David C <dchung20315@gmail.com>
2023-05-11 13:28:57 -0700

    retrieve data from the handlebar and put into the song-chart js

--- public/js/song-chart.js
+++ public/js/song-chart.js
@@ -1,13 +1,20 @@
 import Chart from 'chart.js/auto';
-import { getRelativePosition } from 'chart.js/helpers';
+const tempoData = document.querySelector("#tempoData");
+const danceData = document.querySelector("#danceData");
+const valenceData = document.querySelector("#valenceData");
+const speechData = document.querySelector("#speechData");
+const sentimentData = document.querySelector("#sentimentData");
 
-const chart = new Chart(ctx, {
+const chart = new Chart(document.getElementById("SongAnalysis"), {
   type: 'bar',
   data: {
     labels: ['tempo', 'danceability', 'valence', 'speechiness', 'sentiment'],
     datasets: [{
         xAxisID: 'xAxis1',
-        data: [1, 2, 3, 4, 5]
+        data: [tempoData, danceData, valenceData, speechData, sentimentData]
     }]
+  },
+  options: {
+    backgroundColor: '#32CD32'
   }
 });
