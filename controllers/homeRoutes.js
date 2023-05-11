041c67efb8fa22f68a0a2ddf54a198b2adc65348
Acquah <emanuel.molina2k@gmail.com>
2023-05-11 10:31:29 -0700

    add song and playlist id routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -85,4 +85,36 @@ router.get('/song-search', sessionAuth, checkSpotAuth, (req, res) => {
   res.render('song-search', { authorizeURL });
 });
 
+router.get('/playlists/:id', async(req, res) => {
+  try{
+    const playlistsData = await Playlist.findByPk(req.params.id, {
+      include: [{
+        model: Song,
+      }]
+    });
+    const playlist = playlistsData.get({ plain: true });
+    res.render('playlist', {
+      playlist: playlist,
+      authorizeURL,
+      user_id: req.session.userId,
+    })
+  } catch (err){
+    res.status(500).json(err);
+  }
+});
+
+router.get('/songs/:id', async(req, res) => {
+  try{
+    const songData = await Song.findByPk(req.params.id);
+    const song = songData.get({ plain: true });
+    res.render('song', {
+      song: song,
+      authorizeURL,
+      user_id: req.session.userId,
+    })
+  } catch (err){
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
